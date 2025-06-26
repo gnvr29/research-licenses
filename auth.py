9282b849fdb18ddec487554830fa40696ed8e3ad
Kaio Mayer <99258505+kaiomayer@users.noreply.github.com>
2025-06-25 22:30:51 -0300

    feat: Implements authentication function to obtain FOSSology token

---
+++ auth.py
@@ -0,0 +1,36 @@
+import requests
+
+FOSSology_URL = 'http://localhost/api/login'
+
+def authenticate(username, password):
+
+    """
+    Função para autenticar no FOSSology e obtem o token de autenticação.
+    
+    Args:
+    - username (str): Nome de usuário.
+    - password (str): Senha do usuário.
+    
+    Returns:
+    - str: Token de autenticação se sucesso, None se falha.
+    """
+
+    payload = {
+        'username': username,
+        'password': password
+    }
+    
+    try:
+        response = requests.post(FOSSology_URL, json=payload)
+        
+        if response.status_code == 200:
+           
+            token = response.json().get('token')
+            print(f"Autenticação bem-sucedida. Token: {token}")
+            return token  
+        else:
+            print("Falha na autenticação.")
+            return None  
+    except requests.exceptions.RequestException as e:
+        print(f"Erro de conexão: {e}")
+        return None  
